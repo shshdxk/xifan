aa3516f098ecbc95a117777082f9112796e28a96
shshdxk <609022045@qq.com>
2025-08-25 17:00:06 +0800

    还原blockingDeque

--- redisx/src/main/java/io/github/shshdxk/redisx/DelayedMessageService.java
+++ redisx/src/main/java/io/github/shshdxk/redisx/DelayedMessageService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RBlockingDeque;
 import org.redisson.api.RDelayedQueue;
-import org.redisson.api.RQueue;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 
@@ -33,7 +32,7 @@ public class DelayedMessageService {
     /**
      * 延迟队列缓存
      */
-    private final Map<String, RQueue<?>> delayedQueueMap = new HashMap<>();
+    private final Map<String, RDelayedQueue<?>> delayedQueueMap = new HashMap<>();
 
     /**
      * 添加处理器,处理器不会自动被添加,项目初始化时请添加全部的处理器,不然会导致没添加处理器时消息无法被处理
@@ -43,13 +42,13 @@ public class DelayedMessageService {
      */
     public <T> void addHandler(DelayedMessageHandler<T> handler, Class<T> clazz) {
         String key = handler.getClass() + "." + clazz.getName();
-        RQueue<T> rQueue;
+        RDelayedQueue<T> delayedQueue;
         if (!delayedQueueMap.containsKey(key)) {
             RBlockingDeque<T> blockingDeque = redissonClient.getBlockingDeque(key);
-            rQueue = redissonClient.getQueue(key);
+            delayedQueue = redissonClient.getDelayedQueue(blockingDeque);
             blockingDequeHashMap.put(key, blockingDeque);
-            delayedQueueMap.put(key, rQueue);
-            Executors.newSingleThreadExecutor().submit(() -> this.processMessages(rQueue, blockingDeque, handler));
+            delayedQueueMap.put(key, delayedQueue);
+            Executors.newSingleThreadExecutor().submit(() -> this.processMessages(delayedQueue, blockingDeque, handler));
         }
     }
 
@@ -62,38 +61,36 @@ public class DelayedMessageService {
      */
     public <T> void addMessage(DelayedMessageHandler<T> handler, T message, long delaySeconds) {
         String key = handler.getClass() + "." + message.getClass();
-        RQueue<T> rQueue;
+        RDelayedQueue<T> delayedQueue;
         if (delayedQueueMap.containsKey(key)) {
             try {
-                rQueue = (RQueue<T>) delayedQueueMap.get(key);
+                delayedQueue = (RDelayedQueue<T>) delayedQueueMap.get(key);
             } catch (Exception e) {
                 log.error("已存在的类型和加入消息的类型不匹配");
                 throw new RuntimeException(e);
             }
         } else {
             RBlockingDeque<T> blockingDeque = redissonClient.getBlockingDeque(key);
-            rQueue = redissonClient.getQueue(key);
+            delayedQueue = redissonClient.getDelayedQueue(blockingDeque);
             blockingDequeHashMap.put(key, blockingDeque);
-            delayedQueueMap.put(key, rQueue);
-            Executors.newSingleThreadExecutor().submit(() -> this.processMessages(rQueue, blockingDeque, handler));
+            delayedQueueMap.put(key, delayedQueue);
+            Executors.newSingleThreadExecutor().submit(() -> this.processMessages(delayedQueue, blockingDeque, handler));
         }
-        RDelayedQueue<T> delayedQueue = redissonClient.getDelayedQueue(rQueue);
         delayedQueue.offer(message, delaySeconds, TimeUnit.SECONDS);
     }
 
     /**
      * 处理延迟消息
-     * @param rQueue 延迟队列
+     * @param delayedQueue 延迟队列
      * @param blockingDeque 阻塞队列
      * @param handler 处理器
      * @param <T> 消息类型
      */
-    public <T> void processMessages(RQueue<T> rQueue, RBlockingDeque<T> blockingDeque, DelayedMessageHandler<T> handler) {
+    public <T> void processMessages(RDelayedQueue<T> delayedQueue, RBlockingDeque<T> blockingDeque, DelayedMessageHandler<T> handler) {
         while (true) {
             try {
                 T message = blockingDeque.take();
                 if (!handler.run(blockingDeque, message)) {
-                    RDelayedQueue<T> delayedQueue = redissonClient.getDelayedQueue(rQueue);
                     delayedQueue.offer(message, 1, TimeUnit.SECONDS);
                 }
             } catch (InterruptedException e) {
